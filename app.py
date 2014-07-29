ae21a731ad9be090c4f22644b72493d6a426065e
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-29 13:10:27 +0100

    Create basic flask app

---
+++ app.py
@@ -0,0 +1,26 @@
+import yaml
+from flask import Flask
+
+# Configuration #
+
+try:
+	config = yaml.load(open('config.yml', 'r'))
+except:
+	raise Exception('Invalid config.yml')
+
+if isinstance(config, type(None)):
+	raise Exception('Invalid config.yml')
+
+# Application #
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello, world!'
+
+# Server #
+
+if __name__ == '__main__':
+	app.run(debug = True, host = '0.0.0.0')
+
