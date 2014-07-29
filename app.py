110fd02aedef276c68c924bf77f9de68c477f49d
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-29 13:52:42 +0100

    Create checks for types and remove default route

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import yaml
 from flask import Flask
+import router
 
 # Configuration #
 
 try:
 	config = yaml.load(open('config.yml', 'r'))
+
+	if not isinstance(config, dict):
+		raise Exception()
 except:
 	raise Exception('Invalid config.yml')
+
+if not isinstance(config['flask'], dict):
+	raise Exception('Missing flask from config')
 
 # Application #
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-	return 'Hello, world!'
 
 # Server #
 
