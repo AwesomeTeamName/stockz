70a3d731282de82de2ab63ad162d0fcdbbfa97dc
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-30 15:24:13 +0100

    Make command execution dynamic

--- routes/twilio.py
+++ routes/twilio.py
@@ -10,12 +10,20 @@ twiml = """<?xml version="1.0" encoding="UTF-8" ?>
     <Message>{0}</Message>
 </Response>"""
 
+def generate_response(message):
+	return Response(twiml.format(message), mimetype='text/xml')
+
 @blueprint.route('/twilio', methods = ['GET', 'POST'])
 def twilio():
+	if not 'Body' in request.form:
+		return generate_response('Something went wrong!')
+
+	user_message = request.form['Body']
+
 	try:
-		message = client.execute('hello')
+		message = client.execute(user_message)
 	except:
 		message = 'Something went wrong!'
 
 	response = twiml.format(message)
-	return Response(response, mimetype='text/xml')
+	return generate_response(message)
