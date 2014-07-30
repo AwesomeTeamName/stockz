0a4a83df7294c77fd40f1c49249409a9ca942184
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-30 13:58:10 +0100

    Change response type

--- routes/twilio.py
+++ routes/twilio.py
@@ -1,10 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 
 blueprint = Blueprint('twilio', __name__)
 
 @blueprint.route('/twilio')
 def twilio():
-	return """<?xml version="1.0" encoding="UTF-8" ?>
+	response = """<?xml version="1.0" encoding="UTF-8" ?>
 <Response>
     <Message>I like cats!</Message>
 </Response>"""
+	return Response(response, mimetype='text/xml')
