05273d191d3402941a2c620bbadacee8a590db00
Jack Wilsdon <jack.wilsdon2@gmail.com>
2014-07-30 14:07:12 +0100

    Add echo server

--- routes/twilio.py
+++ routes/twilio.py
@@ -1,11 +1,14 @@
-from flask import Blueprint, Response
+from flask import Blueprint, request, Response
 
 blueprint = Blueprint('twilio', __name__)
 
+twiml = """<?xml version="1.0" encoding="UTF-8" ?>
+<Response>
+    <Message>{0}</Message>
+</Response>"""
+
 @blueprint.route('/twilio')
 def twilio():
-	response = """<?xml version="1.0" encoding="UTF-8" ?>
-<Response>
-    <Message>I like cats!</Message>
-</Response>"""
+	message = request.form['Body']
+	response = twiml.format(message)
 	return Response(response, mimetype='text/xml')
